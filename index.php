c8525d6239c0d582653029e37cf27cfe2f7ede2f
ubuntu <ubuntu@localhost.localdomain>
2021-05-30 16:30:59 +0800

    测试

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+echo phpinfo();
+exit();
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
